f36caae895b0e4ef51dde47afa2bc585add5d872
jsw <l.lllkj@163.com>
2015-07-28 22:15:39 +0800

    write some stub

--- controller.js
+++ controller.js
@@ -4,7 +4,7 @@ var EventEmitter = require('events').EventEmitter;
 
 tg.on('message', function (msg) {
     if (msg.chat.id < 0) {
-
+        
         // ****************************
         // Events for Group Changes
         if (msg.new_chat_participant) {
@@ -40,33 +40,73 @@ tg.on('message', function (msg) {
         }
         // OK end.
         // ****************************
-
+        
         // ****************************
         // Begin InGroup Command Process
-        if (msg.text && (msg.text.slice(0,1) == '/')) {
+        if (msg.text && (msg.text.slice(0, 1) == '/')) {
             var strget = msg.text.split(' ');
+            var ret = {
+                group: Math.abs(msg.chat.id),
+                user: msg.from,
+                timestamp: msg.date
+            };
             switch (strget[0]) {
                 case "/kickme":
+                    ret.target = msg.from;
+                    ret.type = "kick";
+                    ret.require_permission: "anyone";
+                    EventEmitter.emit('managed_cmd_request', ret);
+                    break;
                 case "/help":
 
                 // Normal Managed Group Part
                 case "/modkick":
+                    if (msg.reply_to_message) {
+                        ret.target = msg.reply_to_message.from;
+                        ret.type = "kick";
+                        ret.require_permission: "moderator";
+                        EventEmitter.emit('managed_cmd_request', ret);
+                    }
+                    break;
                 case "/modban":
+                    if (msg.reply_to_message) {
+                        ret.target = msg.reply_to_message.from;
+                        ret.type = "ban";
+                        ret.require_permission: "moderator";
+                        EventEmitter.emit('managed_cmd_request', ret);
+                    }
+                    break;
                 case "/rules":
                 case "/description":
                 case "/promote":
+                    if (msg.reply_to_message) {
+                        ret.target = msg.reply_to_message.from;
+                        ret.type = "promote";
+                        ret.require_permission: "admin";
+                        EventEmitter.emit('managed_cmd_request', ret);
+                    }
+                    break;
                 case "/demote":
+                    if (msg.reply_to_message) {
+                        ret.target = msg.reply_to_message.from;
+                        ret.type = "demote";
+                        ret.require_permission: "admin";
+                        EventEmitter.emit('managed_cmd_request', ret);
+                    }
+                    break;
                 case "/invite":
+                case "/modlist":
 
                 // Admin Group Part
-                case "/assign-admingroup":
-                case "/unassign-admingroup":
-                case "/modlist":
+                case "/define2admingroup":
+                case "/assign":
+                case "/deassign":
                 case "/ban":
                 case "/unban":
                 case "/kick":
                 case "/add-admin":
                 case "/remove-admin":
+                case "/kickall":
                 case "/join":
                 case "/settings":
 
@@ -79,6 +119,15 @@ tg.on('message', function (msg) {
                 // .... special cases
                 case "/set":
                 case "/list":
+
+                // Headquarter
+                case "/block":
+                    ret.target = msg.from;
+                    ret.type = "kick";
+                    ret.require_permission: "headquarter";
+                    EventEmitter.emit('managed_cmd_request', ret);
+                break;
+                case "/unblock":
             }
         }
 
@@ -99,6 +148,8 @@ var outinterface = {
             });
         });
     },
-    msg,
+    msg: function (msgobj) {
+        return tg.sendMessage(msgobj);
+    }
     newProposal
 }
